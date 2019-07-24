cde69453ce0b5e47968bac09675c786365f40964
Ward, Gretchen <gretchen.ward@nih.gov>
2019-07-24 13:35:57 -0400

    toggle active class on drowdown for arrow

--- js/navigation.js
+++ js/navigation.js
@@ -4,6 +4,7 @@ const dropdownMenu = document.querySelector('.dropdown ul')
 const toggleMenu = () => {
   dropdown.addEventListener('click', (event) => {
     event.preventDefault();
+    dropdown.classList.toggle("active");
     dropdownMenu.classList.toggle("shown");
   })
 
