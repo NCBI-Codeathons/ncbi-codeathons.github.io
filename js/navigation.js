12aec06f039bce98af89010090291480d213aebf
Ward, Gretchen <gretchen.ward@nih.gov>
2019-07-29 09:29:03 -0400

    update navigation.js for mobile menu dropdown + update styling

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,19 @@
 const dropdown = document.querySelector('.dropdown')
 const dropdownMenu = document.querySelector('.dropdown ul')
+const mobileDropdown = document.querySelector('.usa-sidenav-sub_list .dropdown')
+const mobileDropdownMenu = document.querySelector('.usa-sidenav-sub_list .dropdown ul');
 
 const toggleMenu = () => {
   dropdown.addEventListener('click', (event) => {
     event.preventDefault();
     dropdown.classList.toggle("active");
     dropdownMenu.classList.toggle("shown");
+  })
+
+  mobileDropdown.addEventListener('click', (event) => {
+    event.preventDefault();
+    mobileDropdown.classList.toggle("active");
+    mobileDropdownMenu.classList.toggle("shown");
   })
 
   document.addEventListener('click', (event) => {
